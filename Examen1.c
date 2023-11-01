eca035fa64239cd7474acc36a243a107b76eca78
Laboratorio <juan.patino@udla.edu.ec>
2023-11-01 15:45:57 -0500

    Segunda linea de codigo

--- Examen1.c
+++ Examen1.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
+//Construya un programa que lea de forma continua números 
+//introducidos por el usuario y vaya contando los números negativos. El programa se debe detener cuando el usuario ingrese el número cero.
 int main () 
 {
+    int numero;
+    int i=0;
     
 }
