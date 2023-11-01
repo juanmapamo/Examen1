157417a830f08df6480029544c70ee7087a5cd95
Laboratorio <juan.patino@udla.edu.ec>
2023-11-01 16:16:31 -0500

    Linea final del codigo

--- Examen1.c
+++ Examen1.c
@@ -3,8 +3,9 @@
 //introducidos por el usuario y vaya contando los números negativos. El programa se debe detener cuando el usuario ingrese el número cero.
 int main () 
 {
-    int numero, continuar;
+    int numero;
     int contador=0;
+    // Aqui entraras en un ciclo en el cual tu podras ingresar la cantidad de numeros que desees, pero solo se contaran los negativos.
     do
     {
      printf ("Ingrese un numero, solo se contaran los numeros negativos\n.");
@@ -13,7 +14,8 @@ int main ()
      {
         contador++;
      }
-    } while (numero=0);
+    } while (numero!=0);
+    // Aqui te mostrara cuantos numeros negativos psusiste.
     printf ("Los numeros negativos que usted coloco son %d", contador);
     
 }
