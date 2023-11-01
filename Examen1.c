c6f8f7352504d0ae8298edc40d122eae19692902
Laboratorio <juan.patino@udla.edu.ec>
2023-11-01 16:04:28 -0500

    Tercera linea de codigo

--- Examen1.c
+++ Examen1.c
@@ -3,7 +3,17 @@
 //introducidos por el usuario y vaya contando los números negativos. El programa se debe detener cuando el usuario ingrese el número cero.
 int main () 
 {
-    int numero;
-    int i=0;
+    int numero, continuar;
+    int contador=0;
+    do
+    {
+     printf ("Ingrese un numero, solo se contaran los numeros negativos\n.");
+     scanf("%d", &numero);
+     if (numero<0) 
+     {
+        contador++;
+     }
+    } while (numero=0);
+    printf ("Los numeros negativos que usted coloco son %d", contador);
     
 }
